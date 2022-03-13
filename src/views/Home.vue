635267c75a8ca1bee49b37b16a5e7fe35f5ef83f
LoncaricLaura <laura.loncaric032@gmail.com>
2022-03-13 20:39:42 +0100

    modified navbar, added logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-    <div class="home">
-        <img class="mx-auto" alt="Vue logo" src="../assets/logo.gif" />
-        <HelloWorld msg="Naručite tehnički pregled Vašeg vozila!" />
+    <div class="grid grid-cols-3">
+        <div>
+            
+        </div>
+        <div>
+            <img class="mx-auto" alt="Vue logo" src="../assets/logo.gif" />
+            <p>Naručite tehnički pregled Vašeg vozila!</p>
+            <p>Lako i besplatno u samo nekoliko koraka!</p>
+        </div>
+    <div>
+
+    </div>
     </div>
 </template>
 
