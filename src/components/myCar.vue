f5ee1856996a035b22a3cbefb1ac171db2e17efb
LoncaricLaura <laura.loncaric032@gmail.com>
2022-06-01 22:15:04 +0200

    modified myCar

--- src/components/myCar.vue
+++ src/components/myCar.vue
@@ -1,31 +1,36 @@
 <template>
-    <router-link
-        :to="{
-            name: 'aboutCar',
-            params: {
-                carBrand: carBrand,
-                carModel: carModel,
-                identification: identification,
-                registration: registration,
-            },
-        }"
+    <div
+        class="rounded-3xl bg-[#E55050] h-[150px] w-[250px] md:w-[230px] lg:w-[250px] grid grid-rows-2 items-center opacity-90 hover:opacity-100 transition"
     >
-        <div
-            class="rounded-3xl bg-[#E55050] h-[150px] w-[250px] md:w-[230px] lg:w-[250px] grid grid-rows-2 items-center opacity-90 hover:opacity-100 transition"
-        >
-            <div>
+        <div class="flex">
+            <router-link
+                :to="{
+                    name: 'aboutCar',
+                    params: {
+                        carBrand: carBrand,
+                        carModel: carModel,
+                        identification: identification,
+                        registration: registration,
+                    },
+                }"
+            >
                 <img
-                    src="../assets/carIcon.png"
-                    alt="car icon"
-                    class="w-[120px] mx-auto mt-2"
+                    src="../assets/delete.png"
+                    class="absolute flex mt-11 pl-2 h-[32px] hover:h-[34px]"
                 />
-            </div>
-            <div class="font-bold">
-                <p>{{ carBrand }} {{ carModel }}</p>
-                <p>{{ registration }}</p>
-            </div>
+            </router-link>
+
+            <img
+                src="../assets/carIcon.png"
+                alt="car icon"
+                class="w-[120px] mx-auto mt-6"
+            />
         </div>
-    </router-link>
+        <div class="font-bold">
+            <p>{{ carBrand }} {{ carModel }}</p>
+            <p>{{ registration }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
